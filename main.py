2f713b0d71f53e6c22d6f26ae0379c853e19456b
Arjun Jayakumar <arjunjayakumar34@gmail.com>
2026-04-07 20:35:13 +0530

    Refactor file listing to return structured metadata with relative paths

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from pathlib import Path
-keys = []
+from pprint import pprint
 
 
-def print_all_files(path_str):
-    path = Path(path_str)
 
+def print_all_files(path_str,base_path):
+    path = Path(path_str).resolve()
+    base_path = Path(base_path).resolve()
+    keys = {}
     for item in path.iterdir():
         if item.is_file():
-            keys.append(item)
+            keys[item.relative_to(base_path)] = {
+                "name" : item.name,
+                "extension" : item.suffixes,
+                "size" : item.stat().st_size
+            }
         elif item.is_dir():
-            print_all_files(item)
+            keys.update(print_all_files(item,base_path))
         else:
             print("Path does not exist or is something else")
-        return keys
+    return keys
     
 
 
 
 if __name__ == "__main__":
     source_directory = input("Enter the Source Directory: ")
+    source_directory_dict = print_all_files(source_directory,source_directory)
+    pprint(source_directory_dict)
     destination_directory = input("Enter the Destination Directory: ")
-    source_directory_keys = print_all_files(source_directory)
-    destination_directory_keys = print_all_files(destination_directory)
+    destination_directory_dict = print_all_files(destination_directory,destination_directory)
+    pprint(destination_directory_dict)
